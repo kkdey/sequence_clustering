bb3272c8bafe08bfdfe58e93ce6739dc748f38ac
Zhengrong Xing <zrxing@spudhead.localdomain>
2015-05-04 18:26:15 -0500

    added test plots

---
+++ src/analysis_k562ctcf/run_data.R
@@ -0,0 +1,7 @@
+dir.name = "~/projects/sequence_clustering"
+source(file.path(dir.name,"src","cluster_seq_mix.R"))
+library(smash)
+
+load(file.path(dir.name,"results/analysis_k562ctcf","data.sig.Robj"))
+
+
