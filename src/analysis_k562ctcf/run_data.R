f8bbf4b07fcb1290e6f4bcd1147e21361fce0726
zrxing <zrxing@uchicago.edu>
2015-05-31 20:52:31 -0500

    small changes

--- src/analysis_k562ctcf/run_data.R
+++ src/analysis_k562ctcf/run_data.R
@@ -1,8 +1,19 @@
 dir.name = "~/projects/sequence_clustering"
-source(file.path(dir.name,"src","cluster_seq_mix.R"))
+source(file.path("src","cluster_seq_mix.R"))
 library(smash)
 
 load(file.path(dir.name,"results/analysis_k562ctcf","data.sig.Robj"))
 
-sample.data = data.sig[sample(1:dim(data.sig)[1], 100), ]
+
+ind.high = rowMeans(data.sig)>mean(rowMeans(data.sig))
+sample.data = data.sig[ind.high, ][sample(1:sum(ind.high), 500), ]
+sample.data = data.sig[ind.high, ]
 sample.res.smooth = cluster.mix(sample.data, smooth=TRUE, K=10, tol=1e-4, maxit=10000)
+
+
+for(i in 1:10){
+  plot(sample.res.smooth$lambda[i,],type='l',main=paste0("cluster ",i))
+}
+
+
+sample.res.smooth$pi
