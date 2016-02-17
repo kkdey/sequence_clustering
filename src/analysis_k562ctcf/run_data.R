a13fe8618ac03d161b4e0afd85ab983f681cdb4c
zrxing <zrxing@uchicago.edu>
2016-02-17 00:29:30 -0600

    added oas1 results

--- src/analysis_k562ctcf/run_data.R
+++ src/analysis_k562ctcf/run_data.R
@@ -5,6 +5,6 @@ library(smash)
 load(file.path(dir.name,"results/analysis_k562ctcf","data.sig.Robj"))
 
 ind.high = rowMeans(data.sig)>mean(rowMeans(data.sig))
-sample.data = data.sig[ind.high, ][sample(1:sum(ind.high), 2000), ]
+sample.data = data.sig[ind.high, ][sample(1:sum(ind.high), 10000), ]
 print(dim(sample.data))
 sample.res.smooth = cluster.mix(sample.data, smooth=TRUE, K=10, tol=1e-4, maxit=10000)
