db31d174771f2a4a1976d329c8eb10688bc0ea22
zrxing <zrxing@uchicago.edu>
2016-05-13 18:03:28 -0500

    added script to run a large region

---
+++ results/analysis_gtex/run_multiseq_pairs_10_73576054_73611082.R
@@ -0,0 +1,52 @@
+library(multiseq)
+
+
+args = commandArgs(TRUE)
+file = "../../data/gtex/reads_100_10_73576054_73611082.Robj"
+
+region = unlist(strsplit(file, "[/]"))
+region = region[length(region)]
+region = unlist(strsplit(region, "[.]"))
+region = region[1]
+region = unlist(strsplit(region, "_"))
+region = region[-(1:2)]
+region = paste(region, collapse = "_")
+
+print(region)
+print(file)
+
+load(file)
+
+
+res_multiseq = list()
+
+for(i in 1:7){
+  data_temp = rbind(reads[[i]][[2]], reads[[i+1]][[2]])
+  data_temp = data_temp[, 1:(2^15)]
+  g_temp = rep(0:1, each = 100)
+  res_multiseq[[i]] = multiseq(data_temp, g_temp, reflect = FALSE, lm.approx = TRUE)
+}
+
+tissue1 = NULL
+tissue2 = NULL
+for(i in 1:7){
+  temp = reads[[i]][[1]]
+  temp = gsub(" .*$", "", temp)
+  tissue1 = c(tissue1, temp)
+}
+for(i in 2:8){
+  temp = reads[[i]][[1]]
+  temp = gsub(" .*$", "", temp)
+  tissue2 = c(tissue2, temp)
+}
+
+
+for(i in 1:7){
+  pdfname = paste("multiseq_effect_100", tissue1[i], tissue2[i], region, sep = "_")
+  pdf(paste0(pdfname, ".pdf"), height = 6, width = 9)
+  plot(res_multiseq[[i]]$effect.mean, ylab = "effect", main = paste("Effect size estimates between tissues", tissue1[i], "and", tissue2[i], " "), type = 'l')
+  abline(h = 0, lty = 2, col = 2)
+  dev.off()
+}
+
+save(res_multiseq, tissue1, tissue2, file = paste0("multiseq_effect_100_", region, ".Robj"))
