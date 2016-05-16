031d0902748ee3600f7be59214ff5dd28ebaa94f
zrxing <zrxing@uchicago.edu>
2016-05-16 11:11:15 -0500

    minor changes

--- results/analysis_gtex/qsub_run_clustering.sh
+++ results/analysis_gtex/qsub_run_clustering.sh
@@ -2,7 +2,8 @@
 
 MEM=40g
 K=$1
-region=$2
+sample_size=$2
+region=$3
 
 LOG_DIR="$HOME/projects/sequence_clustering/results/analysis_gtex/logs/"
 if [ ! -d "${LOG_DIR}" ]; then
@@ -10,7 +11,7 @@ if [ ! -d "${LOG_DIR}" ]; then
 fi
 
 
-PROCESS_NAME=`echo "run_clustering_$1_$2"`
-echo "/data/tools/R-3.1.1/bin/Rscript run_clustering.R $1 $2" | \
+PROCESS_NAME=`echo "run_clustering_$1_$3"`
+echo "/data/tools/R-3.1.1/bin/Rscript run_clustering.R $1 $2 $3" | \
 	qsub -l h_vmem=${MEM} -v PATH -cwd -N ${PROCESS_NAME} \
 	    -o ${LOG_DIR}${PROCESS_NAME}"_log.out" -e ${LOG_DIR}${PROCESS_NAME}"_log.err"
--- results/analysis_gtex/run_clustering.R
+++ results/analysis_gtex/run_clustering.R
@@ -4,7 +4,8 @@ source("../../src/cluster_seq_mix.R")
 
 args = commandArgs(TRUE)
 K = as.numeric(args[1])
-region = as.character(args[2])
+sample_size = as.numeric(args[2])
+region = as.character(args[3])
 
 file = paste0("reads_100_", region, ".Robj")
 load(file.path("../../data/gtex", file))
@@ -14,8 +15,8 @@ data_matrix = NULL
 tissue_name = NULL
 for(i in 1:8){
   data_temp = reads[[i]][[2]]
-#   sample_ind = sample(1:100, 30)
-#   data_temp = data_temp[sample_ind, ]
+  sample_ind = sample(1:100, sample_size)
+  data_temp = data_temp[sample_ind, ]
   data_matrix = rbind(data_matrix, data_temp)
   tissue_name_temp = reads[[i]][[1]]
   tissue_name_temp = gsub(" .*$", "", tissue_name_temp)
