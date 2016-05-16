4852a1aa22123c16e18e228cc37473722049da2d
zrxing <zrxing@uchicago.edu>
2016-05-16 10:58:02 -0500

    minor chan

--- results/analysis_gtex/run_clustering.R
+++ results/analysis_gtex/run_clustering.R
@@ -3,8 +3,8 @@ source("../../src/cluster_seq_mix.R")
 
 
 args = commandArgs(TRUE)
-K = args[1]
-region = args[2]
+K = as.numeric(args[1])
+region = as.character(args[2])
 
 file = paste0("reads_100_", region, ".Robj")
 load(file.path("../../data/gtex", file))
@@ -14,8 +14,8 @@ data_matrix = NULL
 tissue_name = NULL
 for(i in 1:8){
   data_temp = reads[[i]][[2]]
-  sample_ind = sample(1:100, 5)
-  data_temp = data_temp[sample_ind, ]
+#   sample_ind = sample(1:100, 30)
+#   data_temp = data_temp[sample_ind, ]
   data_matrix = rbind(data_matrix, data_temp)
   tissue_name_temp = reads[[i]][[1]]
   tissue_name_temp = gsub(" .*$", "", tissue_name_temp)
