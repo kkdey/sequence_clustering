8deb016b88a50c6e83b0dd675db8c2adaf1f1fd5
zrxing <zrxing@uchicago.edu>
2016-06-14 14:03:55 -0500

    minor changes

--- src/analysis_oas1/run_default.R
+++ src/analysis_oas1/run_default.R
@@ -5,7 +5,7 @@ data = Robj$M
 data = as.matrix(data$M)
 
 
-res = cluster.mix(data[, 2049:4096], smooth = TRUE, K = 3, tol = 1e-4, maxit = 4000)
+res = cluster.mix(data[, 2049:4096], smooth = TRUE, K = 4, tol = 1e-4, maxit = 4000)
 
 
 
